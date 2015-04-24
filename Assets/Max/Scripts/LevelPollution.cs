4936a93ba66346db13ee99327bcd654472f19d4b
maxfinn <maxfinn67@gmail.com>
2015-04-24 16:16:39 +1000

    Additional Scripts

--- Assets/Max/Scripts/LevelPollution.cs
+++ Assets/Max/Scripts/LevelPollution.cs
@@ -2,14 +2,24 @@
 using System.Collections;
 
 public class LevelPollution : MonoBehaviour {
+	//GameObject gameObject;
 
 	// Use this for initialization
 	void Start () {
-	
+		//StartCoroutine (FadeScreen ());
+		Color color = GetComponent<Renderer>().material.color;
+		color.a = 0.9f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	IEnumerator FadeScreen()
+	{
+		Color color = GetComponent<Renderer>().material.color;
+		color.a = 0.5f;
+		yield return new WaitForSeconds(1);
 	}
 }
