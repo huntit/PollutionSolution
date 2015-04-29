1acc14d0504c1dde6d2b5345b479f460faff6ede
rilakkuma45 <slamtrlb@gmail.com>
2015-04-29 15:45:50 +1000

    Added psudocode
    Added code to GameManager and AirBar

--- Assets/Laura/Scripts/AirBar.cs
+++ Assets/Laura/Scripts/AirBar.cs
@@ -2,21 +2,42 @@
 using System.Collections;
 
 public class AirBar : MonoBehaviour {
+	private float air = 5;
 
 	// Use this for initialization
 	void Start()
 	{
-	
+		air = 100;
+	}
+
+	void FixedUpdate()
+	{
+		//		// consume air while under water 
+		//		if Avatar.inWater
+		//		ReduceAir(amount * time.DeltaTime)
+		//		// if out of air and under water, lose health
+		//		if air <= 0
+		//		HealthBar.ReduceHealth(amount * time.DeltaTime)
+	}
+
+	public void ReduceAir(float amount)
+	{
+//		air = air - amount
+//			DrawAirBar()
+	}
+//			
+	public void IncreaseAir(float amount)
+	{
+//			air = air + amount
+//			DrawAirBar()
 	}
 	
 	// Update is called once per frame
-	void Update()
+
+	void DrawAirBar()
 	{
-	
+		//changes the scale of the air bar
 	}
 
-	public void IncreaseAir(float amount)
-	{
-		Debug.Log(amount.ToString());
-	}
+
 }
--- Assets/Laura/Scripts/GameManager.cs
+++ Assets/Laura/Scripts/GameManager.cs
@@ -1,21 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour {
-
+public class GameManager : MonoBehaviour 
+{
+	private int currentLevel;
 	// Use this for initialization
-	void Start () {
 	
+	//makes sure the HUD elements don't show on the start screen  - code for this is in Awake and Start
+	void Awake()
+	{
+		//Keep this object alive in all scenes (Singleton)
+	}
+
+	void Start () 
+	{
+		//if Current Scene Name == "Start Game" (START SCREEN)
+		//play start music
+		//give option for quit
+		//else if Current Scene Name starts with "Level", then (GAME LEVEL SCENE)
+		//
+		//Set FilterIcons.filterPartsCollected[0..3] to false
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
+	}		
+							
+	public void PauseGame()
+	{
+		//Overlay text to show screen is paused
+		//time.Scale = 0
 	}
-
+								
+	public void ResumeGame()
+	{
+		//delete overlay text
+		//time.Scale = 1
+	}
+								
+	public void LoseGame()
+	{
+		//Load scene "Lose Game"
+		//Display text to tell the Player they have lost
+		//Show options for restart Game or Quit
+					
+		//	if Restart button is pressed
+		//		set current Level = current Level
+		//	if Quit button is pressed
+		//		break all code and close the game
+	}
+												
+	public void LoadNextLevel()
+	{
+		//	currentLevel = currentLevel + 1
+		//	Load Scene currentLevel
+	}
+	
 	public void LevelCompleted()
 	{
-		//Pass
-		Debug.Log ("Works");
+		//	If final level completed, 
+		//	then load scene Win Screen
+		//	Else if next level
+		//	LoadNextLevel()
 	}
 }
