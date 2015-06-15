7646a449bfcaf37c151c0af8ea693e3704814e03
maxfinn <maxfinn67@gmail.com>
2015-06-16 01:39:49 +1000

    Rough Level Complete, Added Parallax Scroll

---
+++ Assets/Max/Scripts/Parallax.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+	public Transform avatar;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(0.695f * avatar.position.x + 14.9f,
+		                                 0.235f * avatar.position.y + 6.5f, 6f);
+	}
+}
