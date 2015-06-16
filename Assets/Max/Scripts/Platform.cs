ce4b6bc7bf2e96d98bc74728111c47d2633bc97a
maxfinn <maxfinn67@gmail.com>
2015-06-16 13:30:39 +1000

    Working Platforms =D

--- Assets/Max/Scripts/Platform.cs
+++ Assets/Max/Scripts/Platform.cs
@@ -27,7 +27,6 @@ public class Platform : MonoBehaviour
 		 */
 		float distCovered = (Time.time - startTime) * speed;
 		float fracJourney = (travelForwards) ? distCovered / journeyLength : (journeyLength - distCovered) / journeyLength;
-		GetComponent<Rigidbody2D>().MovePosition(transform.position + transform.forward * Time.deltaTime);
 
 		transform.position = Vector3.Lerp (pointA.position, pointB.position, fracJourney);
 		if (fracJourney <= 0 || fracJourney >= 1)
@@ -40,7 +39,7 @@ public class Platform : MonoBehaviour
 	// If an object collides with the trigger at the top of the platform, make it move with the platform
 	void OnTriggerEnter2D(Collider2D collider)
 	{
-		collider.transform.parent = gameObject.transform;	// make the object move with this platform by making this its parent 
+		collider.transform.parent = transform;	// make the object move with this platform by making this its parent 
 	}
 
 	// When an object leaves the platform, stop it from moving with the platform
