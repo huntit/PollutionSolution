afa2532d9a1254d3871fbc96f6ff7e813d9b9829
maxfinn <maxfinn67@gmail.com>
2015-06-16 21:49:06 +1000

    Mostly done setting up LevelSetup

--- Assets/Max/Scripts/Parallax.cs
+++ Assets/Max/Scripts/Parallax.cs
@@ -2,15 +2,25 @@
 using System.Collections;
 
 public class Parallax : MonoBehaviour {
-	public Transform avatar;
+	public Transform cameraTrans, topLeft, bottomRight, worldAdjust;
+	private float xGradient, xIntercept, yGradient, yIntercept;
+
 	// Use this for initialization
 	void Start () {
-		
+		xGradient = ((bottomRight.position.x - 14f) - (topLeft.position.x + 14f))/
+			(bottomRight.position.x - topLeft.position.x);
+	
+		xIntercept = (topLeft.position.x + 14f) - (xGradient * topLeft.position.x) + worldAdjust.position.x;
+
+		yGradient = ((bottomRight.position.y + 14.7f) - (topLeft.position.y - 13.3f))/
+			(bottomRight.position.y - topLeft.position.y);
+
+		yIntercept = (topLeft.position.y + 14.7f) - (xGradient * topLeft.position.y) + worldAdjust.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(0.695f * avatar.position.x + 14.9f,
-		                                 0.235f * avatar.position.y + 6.5f, 6f);
+		transform.position = new Vector3(xGradient * cameraTrans.position.x + xIntercept,
+		                                 yGradient * cameraTrans.position.y + yIntercept, 6f);
 	}
 }
