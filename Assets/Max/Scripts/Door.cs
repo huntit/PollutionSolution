888118fe8da7eb9610c9f55e7309c51932d2f5ac
maxfinn <maxfinn67@gmail.com>
2015-06-17 17:16:00 +1000

    Added Door/Switch Combo

--- Assets/Max/Scripts/Door.cs
+++ Assets/Max/Scripts/Door.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Door : MonoBehaviour
 {
 	public Collider2D doorCollider2D;
-	public GameObject doorFront;
+	//public GameObject doorFront;
 
 	public void OpenDoor()
 	{
--- Assets/Max/Scripts/DoorTrigger.cs
+++ Assets/Max/Scripts/DoorTrigger.cs
@@ -3,16 +3,26 @@ using System.Collections;
 
 public class DoorTrigger : MonoBehaviour {
 	public Door door;
+	private float initialYHeight;
 
-	private void OnTriggerEnter2D(Collider2D collider)
+	private void Start()
+	{
+		initialYHeight = transform.position.y;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
 	{
 		//Animate trigger going down
+		//transform.Translate (0f, -0.05f, 0f);
+		transform.position = new Vector2(transform.position.x, initialYHeight - 0.05f);
 		door.OpenDoor();
 	}
 
-	private void OnTriggerExit2D(Collider2D collider)
+	private void OnTriggerExit2D(Collider2D other)
 	{
 		//Animate trigger going up
+		//transform.Translate (0f, 0.05f, 0f);
+		transform.position = new Vector2(transform.position.x, initialYHeight);
 		door.CloseDoor();
 	}
 }
